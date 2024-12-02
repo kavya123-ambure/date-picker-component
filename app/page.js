1e788594d51f88138021a164aa9f0240209fe7ed
kavya123-ambure <kavyaambure@gmail.com>
2024-12-02 15:13:33 +0530

    commit for date-picker-component

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import './globals.css';
 
 export default function Home() {
   return (
-    <div className=" alignment p-8  gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-purple-300 ">
+    <div className="   gap-16 pt-8 font-[family-name:var(--font-geist-sans)] bg-purple-300 ">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full ">
       <h1 className="font-bold text-4xl text-purple-700 text-center mb-6 bg-purple-100 p-4 rounded-lg shadow-md w-full sm:w-3/4 mx-auto main-font">Date Picker App</h1>
       
@@ -12,17 +12,15 @@ export default function Home() {
             
       
       
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-       
+        
        
       </main>
+      <footer className="text-center mt-8 bg-gray-900 p-5">
+        <p className="text-center text-gray-100 font-bold">© 2024 Date Picker App. All rights reserved.</p>
+        <p className="text-center text-gray-100 text-sm font-semibold pt-2 mb-auto">
+          Made with ❤️ by Kavya Ambure
+          </p>
+      </footer>
      
     </div>
   );
